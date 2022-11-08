8151bce85ad0e9316a36583246a1126093d586a2
Daniel Pressner <daniel.pressner@gmail.com>
2022-11-08 17:29:58 +0100

    added code to access local server

--- Habitica API Client/Habitica API Client/HabiticaServerConfig.swift
+++ Habitica API Client/Habitica API Client/HabiticaServerConfig.swift
@@ -15,10 +15,10 @@ public class HabiticaServerConfig {
     public static let beta = ServerConfiguration(scheme: "https", host: "habitrpg-beta.herokuapp.com", apiRoute: "api/\(Constants.defaultApiVersion)")
     public static let gamma = ServerConfiguration(scheme: "https", host: "habitrpg-gamma.herokuapp.com", apiRoute: "api/\(Constants.defaultApiVersion)")
     public static let delta = ServerConfiguration(scheme: "https", host: "habitrpg-delta.herokuapp.com", apiRoute: "api/\(Constants.defaultApiVersion)")
-    public static let localhost = ServerConfiguration(scheme: "http", host: "192.168.178.52:3000", apiRoute: "api/\(Constants.defaultApiVersion)")
+    public static let localhost = ServerConfiguration(scheme: "http", host: "192.168.3.170:3000", apiRoute: "api/\(Constants.defaultApiVersion)")
     public static let stub = ServerConfiguration(shouldStub: true, scheme: "https", host: "habitica.com", apiRoute: "api/\(Constants.defaultApiVersion)")
     
-    public static var current = production
+    public static var current = localhost
         
     public static var etags: [String: String] = [:]
     
